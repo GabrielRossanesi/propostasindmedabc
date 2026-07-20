07ffc057f83021e6cb691e72810751382a6cd091
GabrielRossanesi <GabrielRossanesi@users.noreply.github.com>
2026-07-20 22:43:27 +0000

    deploy: b5ef2172262a90d8d9a655a37623dd5c47f1fb34

---
+++ _next/static/j0CTZ8Y2MR6iWMKfM1FW0/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Proposta-Sindmed-ABC//_next/:path+",
+        "destination": "/Proposta-Sindmed-ABC/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
