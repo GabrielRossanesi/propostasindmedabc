ec7c60dfe2c72a0849a7de6f1001d4eebf250ea7
GabrielRossanesi <GabrielRossanesi@users.noreply.github.com>
2026-07-20 23:08:49 +0000

    deploy: e023f9b5923e3072190f7d9e91170bf2b163eeea

--- _next/static/9LPl7AzRmy9ROVgMYpWMX/_buildManifest.js
+++ _next/static/9LPl7AzRmy9ROVgMYpWMX/_buildManifest.js
@@ -3,8 +3,8 @@ self.__BUILD_MANIFEST = {
     "afterFiles": [],
     "beforeFiles": [
       {
-        "source": "/Proposta-Sindmed-ABC//_next/:path+",
-        "destination": "/Proposta-Sindmed-ABC/_next/:path+"
+        "source": "/propostasindmedabc//_next/:path+",
+        "destination": "/propostasindmedabc/_next/:path+"
       }
     ],
     "fallback": []
